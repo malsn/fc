18e0e7e9de07aabf9c56c854ccee00a30d7f36cf
sergey <malsn@rambler.ru>
2016-06-08 20:05:29 +0300

    basket header

--- src/Application/Sonata/BasketBundle/Controller/BasketController.php
+++ src/Application/Sonata/BasketBundle/Controller/BasketController.php
@@ -101,7 +101,7 @@ class BasketController extends Controller
             array(
                 'type' => 'basket',
                 'element' => $element_id,
-                'totalPrice' => $this->get('sonata.basket')->getTotal() + $this->get('sonata.basket')->getVatAmount(),
+                'totalPrice' => ($this->get('sonata.basket')->getTotal() + $this->get('sonata.basket')->getVatAmount())." руб.",
                 'countElements' => $this->get('sonata.basket')->countBasketElements()
             )
         );
