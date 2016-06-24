9efca48493b3527d67f15c7a39fb081037b719ae
sergey <malsn@rambler.ru>
2016-06-24 15:53:25 +0300

    chancge delivery

---
+++ src/Application/Sonata/DeliveryBundle/Model/FreeCourierDelivery.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Application\Sonata\DeliveryBundle\Model;
+
+use Sonata\Component\Delivery\BaseServiceDelivery;
+
+
+/**
+ * Class TakeAwayDelivery
+ *
+ * Custom delivery class example
+ *
+ * @package Application\Sonata\DeliveryBundle\Model
+ *
+ * @author Hugo Briand <briand@ekino.com>
+ */
+class FreeCourierDelivery extends BaseServiceDelivery
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function isAddressRequired()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCode()
+    {
+        return 'free_courier';
+    }
+
+    public function getName()
+    {
+        return 'Бесплатная курьерская доставка';
+    }
+}
