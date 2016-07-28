b0c3fc8f82818a500e53a6e42d8dce54458a5395
sergey <malsn@rambler.ru>
2016-07-28 09:47:58 +0300

    kladr

--- src/Sonata/Bundle/DemoBundle/Resources/public/js/delivery.js
+++ src/Sonata/Bundle/DemoBundle/Resources/public/js/delivery.js
@@ -6,7 +6,7 @@ function Delivery() {
 
         var form_addr = $('#search_addr');
         var addr_field = form_addr.find('#addr_query');
-        
+
         var deliveryMap = new ymaps.Map("delivery_map", {
             center: [59.96, 30.36],
             zoom: 8,
