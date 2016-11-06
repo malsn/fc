d988c9018e6141b5add0dde4f75ccf84346b0dcd
sergey <malsn@rambler.ru>
2016-11-07 00:28:40 +0300

    new payment add

--- src/Sonata/Bundle/DemoBundle/Payment/Uniteller.php
+++ src/Sonata/Bundle/DemoBundle/Payment/Uniteller.php
@@ -90,7 +90,7 @@ class Uniteller extends BasePayment
             );
         //$routeName = $response->getContent() == 'ok' ? 'url_return_ok' : 'url_return_ko';
 
-        return new Response('<iframe style="width:400px; height: 400px;" name="pay_iframe">'.$response->getContent().'</iframe>');
+        return new Response($response->getContent());
     }
 
     /**
